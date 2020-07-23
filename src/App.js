6b33cd87fcfa645347856f9bb52a1e1874348095
elmkablielidrissisofia <sofia.elidrissi1989@gmail.com>
2020-07-23 15:49:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <div>
+  <div style ={{border: 'solid 1px black', maxWidth: '100vw'}}>   
+ 
+ <h1 className = "title red"> Votre nom ici </h1>   
+   
+
+       <img src ="img.jpg"/> 
+
+
+</div>   
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/RG9TMn1FJzc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+  ); 
 }
 
 export default App;
